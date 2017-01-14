c99afb2e348437fd3ad707e1af5d42e41d7fab55
joseph park <jpark041@gmail.com>
2017-01-14 14:15:00 -0800

    least valuable item

--- ios-nd-alien-adventure-swift-3/Alien Adventure/LeastValuableItem.swift
+++ ios-nd-alien-adventure-swift-3/Alien Adventure/LeastValuableItem.swift
@@ -9,7 +9,15 @@
 extension Hero {
     
     func leastValuableItem(inventory: [UDItem]) -> UDItem? {
-
+        var maybeLeastValuableItem: UDItem?
+        
+        for currentItem in inventory {
+            if maybeLeastValuableItem == nil || (maybeLeastValuableItem?.baseValue)! > currentItem.baseValue {
+                maybeLeastValuableItem = currentItem
+            }
+        }
+        return maybeLeastValuableItem
+    }
 }
 
 // If you have completed this function and it is working correctly, feel free to skip this part of the adventure by opening the "Under the Hood" folder, and making the following change in Settings.swift: "static var RequestsToSkip = 4"
