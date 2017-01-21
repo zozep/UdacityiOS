a264d2ec5c42a47d163710ce5ae41772e269e7d6
joseph park <jpark041@gmail.com>
2017-01-21 15:29:03 -0800

    shuffle string

--- ios-nd-alien-adventure-swift-3/Alien Adventure/ShuffleStrings.swift
+++ ios-nd-alien-adventure-swift-3/Alien Adventure/ShuffleStrings.swift
@@ -9,6 +9,36 @@
 extension Hero {
     
     func shuffleStrings(s1: String, s2: String, shuffle: String) -> Bool {
-        return true
+        if s1.isEmpty && s2.isEmpty && shuffle.isEmpty {
+            return true
+        } else if s1.characters.count + s2.characters.count != shuffle.characters.count {
+            return false
+        } else {
+            
+            var copy1 : String = ""
+            var copy2 : String = ""
+            var char : Character
+            
+            for index in shuffle.characters.indices {
+                char = shuffle[index]
+                
+                for charS1 in s1.characters {
+                    if charS1 == char {
+                        copy1.append(charS1)
+                    }
+                }
+                for charS2 in s2.characters {
+                    if charS2 == char {
+                        copy2.append(charS2)
+                    }
+                }
+            }
+            
+            if copy1 == s1 && copy2 == s2 {
+                return true
+            } else {
+                return false
+            }
+        }
     }
 }
