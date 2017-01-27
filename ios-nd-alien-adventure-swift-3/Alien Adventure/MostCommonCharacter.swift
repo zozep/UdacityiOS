86ecba080be01fd115fc97a43a7e78cffe0cb6d7
joseph park <jpark041@gmail.com>
2017-01-27 12:57:11 -0800

    most common character

--- ios-nd-alien-adventure-swift-3/Alien Adventure/MostCommonCharacter.swift
+++ ios-nd-alien-adventure-swift-3/Alien Adventure/MostCommonCharacter.swift
@@ -9,6 +9,43 @@
 extension Hero {
     
     func mostCommonCharacter(inventory: [UDItem]) -> Character? {
-        return nil
+        
+        /*
+         1. Loop through each item in the inventory and count the number of times each character appears. 
+         2. Also, make sure to use the lowercase representation of each item's name. 
+            Otherwise, an item named "StarDust" would add 1 to separate counts for 'S' and 's'.
+         2. Return the character that appears the most.
+        */
+        
+        var letters = [Character: Int]()
+        
+        if inventory.count > 0 {
+            for item in inventory {
+                let itemNameInLowerCase = item.name.lowercased()
+                
+                for char in itemNameInLowerCase.characters {
+                    //print(char)
+                    
+                    if let existCount = letters[char] {
+                        letters[char] = existCount + 1
+                    } else {
+                        letters[char] = 1
+                    }
+                }
+            }
+        } else {
+            return nil
+        }
+        
+        var chatThatAppearsMost: Character = "a"
+        var max = 0
+        
+        for (letter, number) in letters {
+            if number > max {
+                max = number
+                chatThatAppearsMost = letter
+            }
+        }
+        return chatThatAppearsMost
     }
 }
