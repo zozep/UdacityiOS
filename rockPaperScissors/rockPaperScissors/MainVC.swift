d9df87fbc2eb3f8ab39e41e7b53031376e4f901b
joseph park <jpark041@gmail.com>
2017-02-17 22:11:20 +0700

    2nd VC made, boiler plate

--- rockPaperScissors/rockPaperScissors/MainVC.swift
+++ rockPaperScissors/rockPaperScissors/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  rockPaperScissors
 //
 //  Created by Joseph Park on 2/17/17.
@@ -8,18 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
---
+++ rockPaperScissors/rockPaperScissors/ThrowResultsVC.swift
@@ -0,0 +1,18 @@
+//
+//  ThrowResultsVC.swift
+//  rockPaperScissors
+//
+//  Created by Joseph Park on 2/17/17.
+//  Copyright © 2017 Joseph Park. All rights reserved.
+//
+
+import UIKit
+
+class ThrowResultsVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
