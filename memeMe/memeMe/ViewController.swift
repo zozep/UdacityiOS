18a513827f76a062bc6e482d98bb0a6abae56fad
joseph park <jpark041@gmail.com>
2017-02-20 11:28:10 +0700

    imagePickerVC

--- memeMe/memeMe/ViewController.swift
+++ memeMe/memeMe/ViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imagePickerView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func pickAnImage(_ sender: Any) {
+        let imagePickerController = UIImagePickerController()
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
 
 }
 
