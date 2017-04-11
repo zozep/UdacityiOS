3f1fc4e9d363e3d9cedacdf2603455bc84704623
joseph park <jpark041@gmail.com>
2017-04-10 20:36:52 -0700

    methodParameters for searching by phrase & LatLong set, boundingBox WIP

--- networking exercises/FlickFinder/FlickFinder/ViewController.swift
+++ networking exercises/FlickFinder/FlickFinder/ViewController.swift
@@ -54,8 +54,16 @@ class ViewController: UIViewController {
         if !phraseTextField.text!.isEmpty {
             photoTitleLabel.text = "Searching..."
             // TODO: Set necessary parameters!
-            let methodParameters: [String: AnyObject] = [:]
-            displayImageFromFlickrBySearch(methodParameters)
+            let methodParameters = [
+                Constants.FlickrParameterKeys.Method: Constants.FlickrParameterValues.SearchMethod,
+                Constants.FlickrParameterKeys.APIKey: Constants.FlickrParameterValues.APIKey,
+                Constants.FlickrParameterKeys.Text: phraseTextField.text!,
+                Constants.FlickrParameterKeys.SafeSearch: Constants.FlickrParameterValues.UseSafeSearch,
+                Constants.FlickrParameterKeys.Extras: Constants.FlickrParameterValues.MediumURL,
+                Constants.FlickrParameterKeys.Format: Constants.FlickrParameterValues.ResponseFormat,
+                Constants.FlickrParameterKeys.NoJSONCallback: Constants.FlickrParameterValues.DisableJSONCallback
+            ]
+            displayImageFromFlickrBySearch(methodParameters as [String: AnyObject])
         } else {
             setUIEnabled(true)
             photoTitleLabel.text = "Phrase Empty."
@@ -70,8 +78,16 @@ class ViewController: UIViewController {
         if isTextFieldValid(latitudeTextField, forRange: Constants.Flickr.SearchLatRange) && isTextFieldValid(longitudeTextField, forRange: Constants.Flickr.SearchLonRange) {
             photoTitleLabel.text = "Searching..."
             // TODO: Set necessary parameters!
-            let methodParameters: [String: AnyObject] = [:]
-            displayImageFromFlickrBySearch(methodParameters)
+            let methodParameters = [
+                Constants.FlickrParameterKeys.Method: Constants.FlickrParameterValues.SearchMethod,
+                Constants.FlickrParameterKeys.APIKey: Constants.FlickrParameterValues.APIKey,
+                Constants.FlickrParameterKeys.BoundingBox: bboxString(),
+                Constants.FlickrParameterKeys.SafeSearch: Constants.FlickrParameterValues.UseSafeSearch,
+                Constants.FlickrParameterKeys.Extras: Constants.FlickrParameterValues.MediumURL,
+                Constants.FlickrParameterKeys.Format: Constants.FlickrParameterValues.ResponseFormat,
+                Constants.FlickrParameterKeys.NoJSONCallback: Constants.FlickrParameterValues.DisableJSONCallback
+            ]
+            displayImageFromFlickrBySearch(methodParameters as [String: AnyObject])
         }
         else {
             setUIEnabled(true)
@@ -80,6 +96,10 @@ class ViewController: UIViewController {
     }
     
     // MARK: Flickr API
+    
+    private func bboxString() -> String {
+        
+    }
     
     private func displayImageFromFlickrBySearch(_ methodParameters: [String: AnyObject]) {
         
