5743b9993e813e6513676ce63a21b58bca759096
joseph park <jpark041@gmail.com>
2017-04-29 17:22:54 -0700

    assets imported, VC name changed to MainVC

--- Verified Projects/OnTheMap/OnTheMap/MainVC.swift
+++ Verified Projects/OnTheMap/OnTheMap/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  OnTheMap
 //
 //  Created by Joseph Park on 4/26/17.
@@ -8,18 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
+    
 
 }
 
