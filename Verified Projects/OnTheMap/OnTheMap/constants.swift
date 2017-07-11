a878d3a1860cd7c3a086425713af6d02af511ceb
joseph park <jpark041@gmail.com>
2017-07-11 07:29:38 -0700

    alert message cleanup II

--- Verified Projects/OnTheMap/OnTheMap/constants.swift
+++ Verified Projects/OnTheMap/OnTheMap/constants.swift
@@ -23,14 +23,13 @@ struct URLString {
 
 
 // apikey and parseAppID values
-struct Values{
-    
+struct Values {
     static let parseAppID = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
     static let APIKey = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
     static let contentType = "application/json"
 }
 
-struct Keys{
+struct Keys {
     
     static let parseAppID = "X-Parse-Application-Id"
     static let APIKey = "X-Parse-REST-API-Key"
@@ -39,7 +38,7 @@ struct Keys{
     static let XSRFToken = "X-XSRF-TOKEN"
 }
 
-struct StudentInfoKeys{
+struct StudentInfoKeys {
     
     // Student Location Creation Key
     static let createdAtKey = "createdAt"
@@ -73,7 +72,6 @@ struct StudentInfoKeys{
 
 // Json response keys
 struct JSONResponseKeys {
-    
     static let error = "error"
     static let results = "results"
     static let objectID = "objectId"
@@ -89,29 +87,29 @@ struct JSONResponseKeys {
 }
 
 // Alert Messages
-struct AlertMessage {
+struct AlertMessage{
     
-    static let overWriteLocation = "You Have Already Posted a Student Location. Would You Like to overwite your current location?"
-    static let enterValidCredentilas = "Enter valid credentials."
-    static let invalidCredentials = "Incorrect Username or Password. Enter correct credentials."
+    static let overWriteLocation = "You have already posted a location. Would You Like to Overwrite Your Current Location?"
+    static let enterValidCredentilas = "Please enter valid credentials."
+    static let invalidCredentials = "Incorrect Username or Password. Please enter correct credentials."
     static let noInternetConnection = "Seems like you don't have an internet connection"
-    static let failedRequest = "Sorry we are unable to serve your request. Please try again."
-    static let failedToFetchUserDetails = "Sorry we are unable to fetch user details. Please try again."
-    static let failedToLoadStudentLocations = "Sorry we are unable to load students locations. Please reload it."
+    static let failedRequest = "Sorry we are unable to serve your request.Please try again."
+    static let failedToFetchUserDetails = "Sorry we are unable to fetch user details.Please try again."
+    static let failedToLoadStudentLocations = "Sorry we are unable to load students locations. please reload it."
     static let errorInLogout = "Error in logout. Please try again later."
     static let invalidURL = "Unable to open provided link."
-    static let failedToUpdateStudentLocation = "Sorry we are unable to update your location. Please try again later."
-    static let failedToPostStudentLocation =  "Sorry we are unable to post your location. Please try again later."
+    static let failedToUpdateStudentLocation = "Sorry We are unable to update your location. Please try again later."
+    static let failedToPostStudentLocation =  "Sorry We are unable to post your location. Please try again later."
     static let failedToGeocodeLocation = "Failed to geocode the given location.Please try another address"
     static let errorInPlacemarks = "No Placemarks returned for the given location.Please provide correct location."
 }
 
 // Alert title
-struct AlertTitle {
+struct AlertTitle{
     
     static let alert = "Alert"
     static let overWrite = "Overwrite"
     static let cancel = "Cancel"
     static let invalidURL = "Invalid URL"
-
+    
 }
